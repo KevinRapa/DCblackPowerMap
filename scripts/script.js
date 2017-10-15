d439b68b396c76668e0dba0dc8002cb9df01927f
KevinRapa <kevjrapa@gmail.com>
2017-10-15 12:46:35 -0400

    Added lat, long coords to events, implemented markers, redid marker design

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,14 @@
-var myMap = L.map('map', {doubleClickZoom: false}).setView([40.741895, -73.989308], 16);
+var BEGIN_YEAR = 1961;
+var END_YEAR = 1995;
+var YEAR_RANGE = END_YEAR - BEGIN_YEAR;
+
+var myMap = L.map('map', {
+	doubleClickZoom: false,
+}).setView([38.896846, -77.035353], 12);
+
 window.mobile = false; // If page is in compact mode.
 $("#street_view").hide(); // Street view is visible when the street_view_button is pressed.
+
 
 // Create Leaflet map and add it to the page.
 var simple = L.tileLayer('https://{s}.tile.openstreetmap.se/hydda/full/{z}/{x}/{y}.png', {
@@ -8,13 +16,27 @@ var simple = L.tileLayer('https://{s}.tile.openstreetmap.se/hydda/full/{z}/{x}/{
 	attribution: 'Tiles courtesy of <a href="http://openstreetmap.se/" target="_blank">OpenStreetMap Sweden</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(myMap);
 
+
+// Holds all icons currently being displayed on the map.
+var displayed = [];
+
+
+// An array which holds arrays of event markers that fall within same year.
+// Array is indexed by [event year] - BEGIN_YEAR
+var allMarkers = new Array(YEAR_RANGE);
+
+for (i = 0; i < YEAR_RANGE + 1; i++) {
+	allMarkers[i] = new Array(); 
+}
+
+
 /*
  * Rearranges page elements when the window gets thinner.
  * Basically moves the right half of the table into the left half,
  * then moves THAT element above the table so that it centers on
  * the page.
  */
-var resize_fun = function() {
+var resizeFun = function() {
 	window.mobile = !window.mobile; // Switch mode.
 
 	if ($(this).width() < 1200 && window.mobile) {
@@ -47,24 +69,81 @@ var resize_fun = function() {
 	}
 };
 
+
 /*
  * Removes each marker from the map, then populates the map with all events
  * that fall in [year] and [month]. If month == 'all', then month is irrelevant.
+ * Finally, fits bounds of the map around the points.
  * In the future, may add functionality to filter by event type too, using icons
  * in the legend as buttons.
  */
-var event_query = function(year, month) {
-	
+var eventQuery = function(year, month) {
+	displayed.forEach(function(marker) {
+		marker.remove();
+	});
+
+	while (displayed.length > 0) {
+		displayed.pop();
+	}
+
+	var bounds = new Array(allMarkers[year - BEGIN_YEAR].length);
+
+	allMarkers[year - BEGIN_YEAR].forEach(function(marker) {
+		displayed.push(marker);
+		marker.addTo(myMap);
+		bounds.push(marker.getLatLng());
+	});
+
+	myMap.fitBounds(bounds, {maxZoom: 15, padding: L.point(20,20)});
+
+	if (displayed.length > 0) {
+		displayed[0].fire('click');
+	}
+};
+
+
+/*
+ * Filters all markers on the map so that only the events with attribute [key]
+ * have the value [value].
+ * For example, key could be 'event type' and value could be 'political'.
+ */
+var filterMarkers = function(key, value) {
+
+};
+
+
+/*
+ * Uses index to locate event info from all_data.js and displays the 
+ * information on the page.
+ */
+var displayEvent = function(index) {
+	var event = spreadsheet.events[index];
+	$("#desc_title").text(event.Event_name);
+	$("#desc_body").text(event.Description);
 }
 
-resize_fun(); // If looking at webpage on a phone, rearranges elements.
-$(window).resize(resize_fun);
+
+/*
+ * Deselects each icon on the map.
+ */
+var deselectAll = function() {
+	displayed.forEach(function(marker) {
+		marker.remove();
+		marker.setIcon(marker.icons[0]);
+		marker.addTo(myMap);
+	});
+}
+
+resizeFun(); // If looking at webpage on a phone, rearranges elements.
+$(window).resize(resizeFun);
+
 
 // Removes the intro screen.
 $("#to_map").on("click", function() {
 	$("#intro_screen").remove();
 	$("body").css("overflow", "visible");
 });
+
 
 // Toggles between the map and the street view image.
 $("#street_view_button").on("click", function() {
@@ -82,9 +161,65 @@ $("#street_view_button").on("click", function() {
 	}
 });
 
+
 // Changes year, then calls event_query to update markers.
 $("#slider").on("input", function() {
 	var year = $(this).val();
 	$('#year').html(year);
+	eventQuery(year, $("#month_select option:selected").text());
 });
 
+
+// Instantiate all icon types.
+var p = 'images/icons/';
+
+var fists = [
+	L.icon({iconUrl: p + 'fst_ic_un.png',   iconSize: [25, 50]}),
+	L.icon({iconUrl: p + 'fst_ic_sel.png',  iconSize: [25, 50]})
+];
+var brushes = [
+	L.icon({iconUrl: p + 'brsh_ic_un.png',  iconSize: [47, 70]}),
+	L.icon({iconUrl: p + 'brsh_ic_sel.png', iconSize: [47, 70]})
+];
+var dollars = [
+	L.icon({iconUrl: p + 'dllr_ic_un.png',  iconSize: [30, 70]}),
+    L.icon({iconUrl: p + 'dllr_ic_sel.png', iconSize: [30, 70]})
+];
+var globes = [
+	L.icon({iconUrl: p + 'glb_ic_un.png',   iconSize: [47, 70]}),
+    L.icon({iconUrl: p + 'glb_ic_sel.png',  iconSize: [47, 70]})
+];
+var schools = [ 
+	L.icon({iconUrl: p + 'schl_ic_un.png',  iconSize: [47, 70]}),
+	L.icon({iconUrl: p + 'schl_ic_sel.png', iconSize: [47, 70]})
+];
+
+
+// Select this icon, deselect all others on the map.
+var iconFlip = function() {
+	deselectAll(); // This should be called first.
+	this.remove();
+	this.setIcon(this.icons[1]);
+	displayEvent(this.eventRef);
+	this.addTo(myMap);
+};
+
+
+// Populate allMarkers.
+(function() {
+	for (i = 0; i < spreadsheet.events.length; i++) {
+		var event = spreadsheet.events[i];
+
+		if (event["Start Date (Year)"] && event.Latitude && event.Longitude) {
+			var start = event["Start Date (Year)"];
+			var marker = L.marker([event.Latitude, event.Longitude], {icon: fists[0]});
+			marker.eventRef = i;
+			marker.icons = fists;
+			marker.on('click', iconFlip);
+			allMarkers[start - BEGIN_YEAR].push(marker);
+		}
+	}
+})();
+
+eventQuery(BEGIN_YEAR, "All");
+displayed[0].fire('click');
