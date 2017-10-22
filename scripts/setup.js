3bc477a69c2a16d10f5479b2e10398fd0b97b999
KevinRapa <kevjrapa@gmail.com>
2017-10-22 12:26:07 -0400

    Animated legend buttons

--- scripts/setup.js
+++ scripts/setup.js
@@ -18,12 +18,13 @@ var E_DESC = "Description";
 var E_NAME = "Event_Name";
 var E_LAT  = "Latitude";
 var E_LONG = "Longitude";
+var E_STVW = "Street_View_URL";
 // ------------------------------------------
 
 var mobile = false;  // If page is in compact mode.
 var selected = null; // The currently selected marker.
 var displayed = [];  // All icons currently being displayed on the map.
-var allMarkers = new Array(YEAR_RANGE); // Holds arrays of markers. Indexed by [year] - BEGIN_YR.
+var allMarkers = new Array(YEAR_RANGE); // Holds arrays of markers. Indexed by [year] - BEGIN_YR + offset.
 
 for (i = 0; i <= YEAR_RANGE; i++) {
 	allMarkers[i] = []; // Each element holds a list of events that happen in same year.
@@ -31,38 +32,19 @@ for (i = 0; i <= YEAR_RANGE; i++) {
 
 $("#street_view").hide(); // Street view is visible when the street_view_button is pressed.
 
+
+/*
+ * Adds the tile layer to the map, then adds the map to the page.
+ * TO CHANGE MAP, VISIT https://leaflet-extras.github.io/leaflet-providers/preview/
+ */
 var myMap = L.map('map'); // Create map
 
-// Adds the tile layer to the map, then adds the map to the page.
-// TO CHANGE MAP, VISIT https://leaflet-extras.github.io/leaflet-providers/preview/
 L.tileLayer('https://{s}.tile.openstreetmap.se/hydda/full/{z}/{x}/{y}.png', {
 	maxZoom: 18,
 	attribution: 'Tiles courtesy of <a href="http://openstreetmap.se/" \
 		target="_blank">OpenStreetMap Sweden</a> &mdash; Map data &copy; \
 		<a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(myMap);
-
-
-/*
- * Fires when a slider changes. Changes year text and switches up markers.
- */
-var changeYear = function() {
-	var yr = $(this).val();
-	$('#year').text(yr);
-	$('#mobile_year').text(yr).animate({
-		left: (((yr - BEGIN_YR) * 15) + "px")
-	}, 15, 'linear');
-
-		//.css("left", ((yr - BEGIN_YR) * 15) + "px"); // Moves year text.
-
-	if (selected) {
-		selected.remove();
-		selected.setIcon(selected.ICONS[0]);
-		selected = null; // Not needed, just for consistency.
-	}
-
-	eventQuery(yr, $("#month_select option:selected").text());
-};
 
 
 /*
@@ -78,47 +60,33 @@ $(window).resize(function() {
 		// Tranforms into compact mode.
 		mobile = ! mobile; // Switch mode.
 		var yr = $("#year").text();
-		
-		if (b.text() === ST_VIEW_SELECTED) {
-			b.trigger('click', true); // Prevents map seizing.
-		}
+
+		b.text() == ST_VIEW_SELECTED && b.trigger('click', true); // Prevents map seizing.
 
 		$("#legend")
 			.after($("#right_box").detach())
-			.before('<input type="range" id="mobile_slider" \
+			.before('<input type="range" id="mobile_slider" class="fade_group" \
 				min="1961" max="1995" value="' + yr + '"/>')
 			.css("margin-bottom", "10px");
-
 		$("#title_box")
 			.after($("#mobile_holder").detach())
 			.text(MOBILE_TTL);
-
 		$("#slider_box").css("visibility", "hidden");
-
 		$("#mobile_slider")
-			.before('<div id="mobile_year">' + yr + '</div>')
+			.before('<div id="mobile_year" class="fade_group">' + yr + '</div>')
 			.on('input', changeYear);
-
 		$("#mobile_year").css("left", ((yr - BEGIN_YR) * 15) + "px");
 		$("#button_and_address").css("bottom", "371px");
 		$("#address").css("bottom", "0px");
-
-		$(".purple_box").each(function() {
-			$(this).css("width", "650px");
-		});
+		$(".purple_box").css("width", "650px");
 	}
 	else if ($(this).width() >= 1200 && mobile) {
 		// Transforms into desktop mode.
 		mobile = ! mobile; // Switch mode.
+		
+		b.text() == ST_VIEW_SELECTED && b.trigger('click', true);
 
-		if (b.text() === ST_VIEW_SELECTED) {
-			b.trigger('click', true);
-		}
-
-		$(".purple_box").each(function() {
-			$(this).css("width", "1200px");
-		});
-
+		$(".purple_box").css("width", "1200px");
 		$("#legend").css("margin-bottom", "");
 		$("#slider").val($("#year").text());
 		$("#mobile_slider").off('input').detach();
@@ -137,17 +105,16 @@ $(window).resize(function() {
 
 /*
  * Removes each marker from the map, then populates the map with all events
- * that fall in [year] and [month]. If month == 'all', then month is irrelevant.
- * Finally, fits bounds of the map around the points.
+ * that fall in [year]. Fits bounds of the map around the points.
  */
-var eventQuery = function(year, month) {
+var eventQuery = function(year) {
 	for (i = 0; i < displayed.length; i++) {
 		displayed[i].remove();
 	}
 
-	selected = null; // Not needed, just for consistency.
+	selected = null; 	  // Not needed, just for consistency.
 	displayed.length = 0; // Clears array.
-	var bounds = [];
+	var bounds = [];      // List of coordinates for map panning.
 
 	allMarkers[year - BEGIN_YR].forEach(function(marker) {
 		displayed.push(marker);
@@ -163,7 +130,30 @@ var eventQuery = function(year, month) {
 };
 
 
-// Removes the intro screen.
+/*
+ * Fires when a slider changes. Changes year text and switches up markers.
+ * Global since #mobile_slider is dynamically attached and detached from the DOM.
+ */
+var changeYear = function() {
+	var yr = $(this).val();
+	$('#year').text(yr);
+	$('#mobile_year').text(yr).animate({
+		left: ((yr - BEGIN_YR) * 15) + "px"
+	}, 15, 'linear');
+
+	if (selected) {
+		selected.remove();
+		selected.setIcon(selected.ICONS[0]);
+		selected = null; // Not needed, just for consistency.
+	}
+
+	eventQuery(yr);
+};
+
+
+/*
+ * Removes the intro screen.
+ */
 $("#to_map").click(function() {
 	$("#intro_box").slideUp();
 	$("#intro_screen").delay(150).fadeOut();
@@ -171,13 +161,13 @@ $("#to_map").click(function() {
 });
 
 
-// Toggles between the map and the street view image.
-$("#street_view_button").on("click", function(e, show = false) {
+/*
+ * Toggles between the map and the street view image.
+ */
+$("#street_view_button").click(function(e, show = false) {
 	if ($(this).text() === ST_VIEW_UNSELECTED) {
 		$(this).text(ST_VIEW_SELECTED);
-		$("#slider_box").fadeOut();
-		$("#mobile_slider").fadeOut();
-		$("#mobile_year").fadeOut();
+		$(".fade_group").fadeOut(); // mobile_slider, mobile_year, slider_box
 		$("#map").fadeOut(function() {
 			$("#street_view").fadeIn();
 		});
@@ -189,52 +179,78 @@ $("#street_view_button").on("click", function(e, show = false) {
 			// Animation causes seizing when screen resizes.
 			$("#street_view").hide();
 			$("#map").show();
-			$("#slider_box").show();
-			$("#mobile_slider").show();
-			$("#mobile_year").show();
+			$(".fade_group").show();
 		}
 		else {
 			$("#street_view").fadeOut(function() {
 				$("#map").fadeIn();
-				$("#slider_box").fadeIn();
-				$("#mobile_slider").fadeIn();
-				$("#mobile_year").fadeIn();
+				$(".fade_group").fadeIn();
 			});
 		}
 	}
 });
 
 
-// Changes year, then calls event_query to update markers.
+/*
+ * Changes year, then calls event_query to update markers.
+ */
 $("#slider").on("input", changeYear);
 
 
-// Instantiate all icon types.
-var FISTS = [
-	L.icon({iconUrl: ICN_PTH + 'fst_ic_un.png',   iconSize: [30, 63]}), // Unselected icon.
-	L.icon({iconUrl: ICN_PTH + 'fst_ic_sel.png',  iconSize: [30, 63]})  // Selected icon.
-];
-var BRUSHES = [
-	L.icon({iconUrl: ICN_PTH + 'brsh_ic_un.png',  iconSize: [47, 70]}),
-	L.icon({iconUrl: ICN_PTH + 'brsh_ic_sel.png', iconSize: [47, 70]})
-];
-var DOLLARS = [
-	L.icon({iconUrl: ICN_PTH + 'dllr_ic_un.png',  iconSize: [30, 70]}),
-    L.icon({iconUrl: ICN_PTH + 'dllr_ic_sel.png', iconSize: [30, 70]})
-];
-var GLOBES = [
-	L.icon({iconUrl: ICN_PTH + 'glb_ic_un.png',   iconSize: [47, 70]}),
-    L.icon({iconUrl: ICN_PTH + 'glb_ic_sel.png',  iconSize: [47, 70]})
-];
-var SCHOOLS = [ 
-	L.icon({iconUrl: ICN_PTH + 'schl_ic_un.png',  iconSize: [47, 70]}),
-	L.icon({iconUrl: ICN_PTH + 'schl_ic_sel.png', iconSize: [47, 70]})
-];
-
-
+/*
+ * Animates legend icons when interacted with.
+ * Clicking them filters markers by event type.
+ */
 (function() {
-	// Deselects currently selected marker, then selects this one.
-	// Fast is false if event is caused from the slider moving.
+	var clickE = function(e) {
+		$(".icon_text").css("font-style", "normal");
+		$(this).next().css("font-style", "oblique");
+		$(this).animate({bottom: "-6px"}, 90)
+			.animate({bottom: "4px"}, 90).animate({bottom: "0px"}, 90);
+	}
+	var hvrInE = function() {
+		$(this).animate({bottom: "4px"}, 90);
+	}
+	var hvrOutE = function() {
+		$(this).animate({bottom: "0px"}, 90);
+	}
+	$(".icon_button").click(clickE).hover(hvrInE, hvrOutE);
+})();
+
+
+/*
+ * Load markers and display first year.
+ */
+(function() {
+
+	// Instantiate all icon types.
+	var FISTS = [
+		L.icon({iconUrl: ICN_PTH + 'fst_ic_un.png',   iconSize: [30, 63]}), // Unselected icon.
+		L.icon({iconUrl: ICN_PTH + 'fst_ic_sel.png',  iconSize: [30, 63]})  // Selected icon.
+	];
+	var BRUSHES = [
+		L.icon({iconUrl: ICN_PTH + 'brsh_ic_un.png',  iconSize: [47, 70]}),
+		L.icon({iconUrl: ICN_PTH + 'brsh_ic_sel.png', iconSize: [47, 70]})
+	];
+	var DOLLARS = [
+		L.icon({iconUrl: ICN_PTH + 'dllr_ic_un.png',  iconSize: [30, 70]}),
+	    L.icon({iconUrl: ICN_PTH + 'dllr_ic_sel.png', iconSize: [30, 70]})
+	];
+	var GLOBES = [
+		L.icon({iconUrl: ICN_PTH + 'glb_ic_un.png',   iconSize: [47, 70]}),
+	    L.icon({iconUrl: ICN_PTH + 'glb_ic_sel.png',  iconSize: [47, 70]})
+	];
+	var SCHOOLS = [ 
+		L.icon({iconUrl: ICN_PTH + 'schl_ic_un.png',  iconSize: [47, 70]}),
+		L.icon({iconUrl: ICN_PTH + 'schl_ic_sel.png', iconSize: [47, 70]})
+	];
+
+
+	/*
+	 * Deselects currently selected marker, then selects this one.
+	 * Fast is false if event is caused from the slider moving.
+	 * This is bound to each marker.
+	 */
 	var iconFlip = function(speed = {fast: false}) {
 		if (selected == this) return; // User clicked the same one twice.
 
@@ -276,6 +292,7 @@ var SCHOOLS = [
 					.css('bottom', mobile ? "0px" : ($(this).height() - 26) + "px").fadeIn(200);
 			});
 		}
+		$("#street_view iframe").attr("src", e[E_STVW] || "");
 	};
 
 	// Populate allMarkers.
@@ -307,6 +324,6 @@ var SCHOOLS = [
 		}
 	}
 
-	eventQuery(BEGIN_YR, "All"); // Default year to start with is first year.
+	eventQuery(BEGIN_YR);
 })();
 
